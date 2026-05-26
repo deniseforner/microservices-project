a33378c62a3d05921f30fa714136649493f42f37
deniseforner <deniseforner97@gmail.com>
2026-05-26 23:52:56 +0100

    chore: add real terraform file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
-resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-bucket-${random_id.suffix.dec}"
+resource "aws_s3_bucket" "test" {
+  bucket = "test-bucket-${random_id.suffix.dec}"
 }
 
 resource "random_id" "suffix" {
